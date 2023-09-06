dc7fedda66b75b51b6cbe50d3315a15b16efb6bf
Paul <eeddll1505@gmail.com>
2023-09-06 16:34:17 +0300

    added sadFace if there is not collections

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,7 @@
+import SadFace from '@/components/icons/SadFace';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Skeleton } from '@/components/ui/skeleton';
+import prisma from '@/lib/prisma';
 import { wait } from '@/lib/wait';
 import { UserButton, currentUser } from '@clerk/nextjs';
 import { Suspense } from 'react';
@@ -8,6 +11,9 @@ export default async function Home() {
     <>
       <Suspense fallback={<WelcomeMsgFallback />}>
         <WelcomeMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading collections</div>}>
+        <CollectionList />
       </Suspense>
     </>
   );
@@ -20,7 +26,7 @@ async function WelcomeMsg() {
     return <div>error</div>;
   }
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <h1 className="text-4xl font-bold">
         Welcome, <br />
         {user.firstName} {user.lastName}
@@ -31,11 +37,30 @@ async function WelcomeMsg() {
 
 function WelcomeMsgFallback() {
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <h1 className="text-4xl font-bold">
         <Skeleton className="w-[150px] h-[36px]" />
         <Skeleton className="w-[150px] h-[36px]" />
       </h1>
     </div>
   );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <Alert>
+        <SadFace />
+        <AlertTitle>There are no collections yet!</AlertTitle>
+        <AlertDescription>Create a collection to get started</AlertDescription>
+      </Alert>
+    );
+  }
 }
